669e563e527a927be77a1b6e543d59395f11d6a5
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2018-02-05 17:13:27 +0100

    Set the paths according to where the files are shipped
     * Code goes to /usr/share/php/passbolt-api/
     * Configuration goes to /etc/passbolt/
     * Variable files (cold cache and GPG keys) go to /var/lib/passbolt/
     * Logs go to /var/log/passbolt/

--- bin/cake.php
+++ bin/cake.php
@@ -6,5 +6,5 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use App\Application;
 use Cake\Console\CommandRunner;
 // Build the runner with an application and root executable name.
-$runner = new CommandRunner(new Application(dirname(__DIR__) . '/config'), 'cake');
+$runner = new CommandRunner(new Application('/etc/passbolt'), 'cake');
 exit($runner->run($argv));
