8b032fbcc502f73bc6e2e0ed6e1951a432e7e1b4
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2018-02-05 17:13:27 +0100

    Set the paths according to where the files are shipped
     * Code goes to /usr/share/php/passbolt-api/
     * Configuration goes to /etc/passbolt/
     * Variable files (cold cache and GPG keys) go to /var/lib/passbolt/
     * Logs go to /var/log/passbolt/

--- webroot/index.php
+++ webroot/index.php
@@ -14,7 +14,7 @@
  * @since         0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-require dirname(__DIR__) . '/config/requirements.php';
+require '/etc/passbolt/requirements.php';
 
 // for built-in server
 if (php_sapi_name() === 'cli-server') {
@@ -32,7 +32,7 @@ use App\Application;
 use Cake\Http\Server;
 
 // Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
+$server = new Server(new Application('/etc/passbolt'));
 
 // Run the request/response through the application
 // and emit the response.
